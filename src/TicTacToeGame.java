6668dc1e4b87dd98003a9dc76b6ee1b6d5f90144
Indhirakumar <indhirakumar49@gmail.com>
2026-09-25 11:13:25 +0530

    fix: fix the errors in the game

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class TicTacToeGame {
-    public static void main(String[]  args) {
+    public static void main(String[] args) {
+        //--------------Initial array------------
         Scanner scanner=new Scanner(System.in);
         char[][] charArray=new char[3][3];
         for(int i=0;i<charArray.length;i++){
@@ -9,66 +10,102 @@ public class TicTacToeGame {
                 charArray[i][j] = ' ';
             }
         }
+        //----------------------------------------
+
         char player='X';
-//
+
         boolean gameOver=false;
-        while(!gameOver){
+        //------------operation---------
+        while(!isgameOver(charArray,player)){
             showArray(charArray);
-            System.out.println("enter Your row: "+player);
-            int a=scanner.nextInt();
-            System.out.println("enter Your column: "+player);
-            int b=scanner.nextInt();
-            charArray[a][b]=player;
-            if(player=='X'){
-                player='O';
-            }else {
-                player='X';
+            System.out.println("enter Your row and column Player: "+player+" = ");
+            int a=scanner.nextInt()-1;
+            int b=scanner.nextInt()-1;
+            System.out.println();
+            if(charArray[a][b]=='X'||charArray[a][b]=='O'){
+                System.out.println("Invalid choise please try again");
             }
-            gameOver=isgameOver(charArray,player);
+            else{
+                charArray[a][b]=player;
+//                if(player=='X'){
+//                    player='O';
+//                }else {
+//                    player='X';
+//                }
+                if(!isgameOver(charArray,player)){
+                    if(player=='X'){
+                        player='O';
+                    }else {
+                        player='X';
+                    }
+                }else {
+                    break;
+                }
+
+            }
+
         }
+        showArray(charArray);
         if(player=='X'){
-            System.out.println("player X is win");
+            System.out.println("Player X is win");
         }else{
-            System.out.println("player O is win");
+            System.out.println("Player O is win");
         }
-//
-//        char charArray[][] ={
-//                {'O','O','X'},
-//                {'O','X','O'},
-//                {'X','O','X'}
-//        };
-//        showArray(charArray);
-//        System.out.println(isgameOver(charArray,'X'));
+
     }
 
+    //------------------------------------------------
+    //------------------------show array--------------
+    //----------------------------------------------
     public static void showArray(char[][] charArray){
+        System.out.println("-------------");
         for(int i=0;i<charArray.length;i++){
+            System.out.print("| ");
             for(int j=0;j<charArray[i].length;j++) {
                 System.out.print(charArray[i][j]);
-                System.out.print("|");
+                System.out.print(" | ");
             }
             System.out.println();
         }
+        System.out.print("-------------");
+        System.out.println();
+
     }
 
+    //---------------------------------------------
+    //---------------find player is win or not------
+    //----------------------------------------------
     public static boolean isgameOver(char[][] charArray,char user){
-        for(int row=0;row< charArray.length;row++){
-            if(charArray[row][0]==user&&charArray[row][1]==user&&charArray[row][2]==user) {
-                return true;
-            }
-        }
-        for(int col=0;col<charArray.length;col++){
-            if(charArray[0][col]==user&&charArray[1][col]==user&&charArray[2][col]==user) {
-                return true;
-            }
-        }
-        if(charArray[0][0]==user&&charArray[1][1]==user&&charArray[2][2]==user){
-            return true;
-        }
-        if(charArray[0][2]==user&&charArray[1][1]==user&&charArray[2][0]==user){
-            return true;
-        }
 
+        //----------check the rows---------------
+        for(int row=0;row< charArray.length;row++){
+            if(charArray[row][0]==user&&
+                    charArray[row][1]==user&&
+                    charArray[row][2]==user) {
+                return true;
+            }
+        }
+        //----------------check the column--------------
+        for(int col=0;col<charArray.length;col++){
+            if(charArray[0][col]==user&&
+                    charArray[1][col]==user&&
+                    charArray[2][col]==user) {
+                return true;
+            }
+        }
+        //--------------check the right to left diagonal---------
+        if(charArray[0][0]==user&&
+                charArray[1][1]==user&&
+                charArray[2][2]==user){
+            return true;
+        }
+        //-----------------check the left to right diagonal--------
+        if(charArray[0][2]==user&&
+                charArray[1][1]==user&&
+                charArray[2][0]==user){
+            return true;
+        }
+        // ----------if no result-------------
         return false;
     }
 }
